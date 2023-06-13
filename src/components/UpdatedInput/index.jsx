0d33cb8c42965fc336b0f16fbe0596c0da943ad4
IamSirious <sirgey.romanenko@yandex.ru>
2023-06-13 19:06:19 +0400

    Profile. Корректировка

--- src/components/UpdatedInput/index.jsx
+++ src/components/UpdatedInput/index.jsx
@@ -1,19 +1,24 @@
-import {useState} from "react";
-import {Button, Form} from "react-bootstrap"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form } from "react-bootstrap"
 import { PencilSquare, XSquare, CheckSquare } from "react-bootstrap-icons";
 
 const Input = ({val, isActive, upd, changeActive, name}) => {
     const [inp, setInput] = useState(val);
     return <>
         {!isActive
-            ? <>{name !== "avatar" && val} <Button onClick={() => changeActive(true)}><PencilSquare/></Button></>
+            ? <>{name !== "avatar" && val}
+				<Link className="fs-4 text-warning text-decoration-none" onClick={() => changeActive(true)}>&nbsp;
+					<PencilSquare/>
+				</Link>
+			</>
             : <>
-                <Form.Control value={inp} onChange={(e) => setInput(e.target.value)}/>
-                <Button variant="danger" onClick={() => changeActive(false)}><XSquare/></Button>
-                <Button variant="success" onClick={() => {
-                    changeActive(false)
-                    upd(name, inp)
-                }}><CheckSquare/></Button>
+                <Form.Control className="mb-0" value={inp} onChange={(e) => setInput(e.target.value)}/>
+                <Link className="fs-4 text-danger text-decoration-none" onClick={() => changeActive(false)}><XSquare/></Link>&nbsp;
+                <Link className="fs-4 text-success text-decoration-none" onClick={() => {
+					changeActive(false)
+					upd(name, inp)
+                }}><CheckSquare/></Link>
             </>
         }
     </>
